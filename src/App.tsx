d6a57399cb7517a5066df5f9a16fa18e99072de4
Rita-cedcoss <ritapal@cedcoss.com>
2023-02-22 14:52:26 +0530

    modification done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FigmaForm from './Figma_App/FigmaForm';
-import "./Figma_App/Figma.css"
+import FigmaForm from './components/FigmaForm';
+import "./components/Figma.css"
 function App() {
   return (
     <FigmaForm/>
